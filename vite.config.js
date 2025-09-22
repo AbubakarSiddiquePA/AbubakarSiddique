973bd1b39be991e68481520b102fd217ed0fca0e
webmate2022 <108720371+webmate2022@users.noreply.github.com>
2025-09-22 17:12:53 +0400

    Fix Vite config for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/AbubakarSiddique/", // <-- REPLACE with your repo name, e.g. "/personal_portfolio/"
-  plugins: [react()]
+  plugins: [react()],
 });
